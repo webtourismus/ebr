1704ac706c98194520fafe8c24c3357816840e51
Webtourismus.at <entwicklung@webtourismus.at>
2023-11-02 12:50:04 +0100

    Fix Seekda packages unpublish status set by cron

--- modules/ebr_accommodation_seekda/src/EventSubscriber/MigrationPackagePublishStatus.php
+++ modules/ebr_accommodation_seekda/src/EventSubscriber/MigrationPackagePublishStatus.php
@@ -5,6 +5,7 @@ declare(strict_types = 1);
 namespace Drupal\ebr_accommodation_seekda\EventSubscriber;
 
 use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\Core\KeyValueStore\KeyValueFactoryInterface;
 use Drupal\ebr\EntityBusinessrules;
 use Drupal\ebr_accommodation_seekda\Entity\AccommodationSeekda;
 use Drupal\migrate\Event\MigrateEvents;
@@ -17,15 +18,26 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 class MigrationPackagePublishStatus implements EventSubscriberInterface {
 
   /**
+   * The grace period in seconds for outdated packages between "package changed timestamp" and "last cron timestamp".
+   */
+  private const UNPUBLISH_GRACE_PERIOD = 300;
+
+  /**
    * The EntityType Manager.
    */
   protected EntityTypeManager $entityTypeManager;
 
   /**
+   * The KeyValue
+   */
+  protected KeyValueFactoryInterface $keyValue;
+
+  /**
    * The constructor.
    */
-  public function __construct(EntityTypeManager $entity_type_manager) {
-    $this->entityTypeManager = $entity_type_manager;
+  public function __construct(EntityTypeManager $entityTypeManager, KeyValueFactoryInterface $keyValue) {
+    $this->entityTypeManager = $entityTypeManager;
+    $this->keyValue = $keyValue;
   }
 
   /**
@@ -38,43 +50,25 @@ class MigrationPackagePublishStatus implements EventSubscriberInterface {
   }
 
   /**
-   * Event callback to unpublish packages missing in the API.
+   * Event callback to unpublish outdated packages.
+   *
+   * The migration _must_ run with "update" flag for this to work.
+   * Otherwise active packages will become unpublished too.
    *
    * @param \Drupal\migrate\Event\MigrateImportEvent $event
    *   The migration import event.
    */
   public function unpublish(MigrateImportEvent $event): void {
     $migration = $event->getMigration();
-    if ($migration->id() != '120_seekda_packages_de') {
+    if (strpos($migration->id(), '_seekda_packages_') !== 3) {
       return;
     }
-
-    // @todo remove the state dependant return once drupal.org/i/3130358 is _really_ fixed
-    // BUGFIX_3130358_PART1 post_migration event is triggered once for every row instead once at the end of a complete migration
-    // as a workaround we save timestamp to avoid mulitple runs of this trigger
-    $lastUnpublishRun = \Drupal::state()->get("ebr_accommodation_seekda.last_postmigrationevent_unpulish.{$migration->id()}", 0);
-    $requestTime = \Drupal::service('datetime.time')->getRequestTime();
-    $maxCronRuntime = \Drupal::configFactory()->get('ebr_accommodation_seekda.settings')->get('cron_max_runtime');
-    if ($requestTime < $lastUnpublishRun + $maxCronRuntime) {
-      return;
-    }
-    // end BUGFIX_3130358_PART1
-
-    $source = clone $migration->getSourcePlugin();
-    $activePackages = [];
-    // BUGFIX_3130358_PART2
-    // because the event fired right after the first row due bug, do not rewind() because it also does a next()
-    // $source->rewind();
-    // end BUGFIX_3130358_PART2
-    while ($source->valid()) {
-      $activePackages[] = $source->current()->getSourceProperty('package_code');
-      $source->next();
-    }
+    $lastImportTime = round($this->keyValue->get('migrate_last_imported')->get($migration->id(), 0) / 1000);
 
     $allSeekdaPackages = $this->entityTypeManager->getStorage('node')->loadByProperties([
       'type' => 'package',
       EntityBusinessrules::FIELD_REMOTE_DATASOURCE => AccommodationSeekda::DATASOURCE,
-      'langcode' => 'de',
+      'default_langcode' => 1,
     ]);
 
     foreach ($allSeekdaPackages as $package) {
@@ -82,12 +76,12 @@ class MigrationPackagePublishStatus implements EventSubscriberInterface {
       if (!$remoteId) {
         continue;
       }
-      $package->set('status', (int) in_array($remoteId, $activePackages));
+      $packageUpdateTime = $package->get('changed')->value;
+      if ($packageUpdateTime + self::UNPUBLISH_GRACE_PERIOD > $lastImportTime ) {
+        continue;
+      }
+      $package->set('status', 0);
       $package->save();
     }
-
-    // BUGFIX_3130358_PART3
-    \Drupal::state()->set("ebr_accommodation_seekda.last_postmigrationevent_unpulish.{$migration->id()}", $requestTime);
-    // end BUGFIX_3130358_PART3
   }
 }
